929324988e19238ac8a79ba0cffa9c50295efb82
Liam1017 <elijahturany1415@gmail.com>
2023-02-28 16:38:17 -0600

    2023/02/28 16:38

--- ChaPartialSort/ChaPartialSort.cpp
+++ ChaPartialSort/ChaPartialSort.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
+#include "PartiallySortedList.h"
 using namespace std;
 
+class PartiallySortedString {
+private:
+	string s;
+public:
+	PartiallySortedString(string value) {
+		s = value;
+	}
+
+	bool operator==(const PartiallySortedString& other) {
+		return (other.s == s);
+	}
+
+	bool operator!=(const PartiallySortedString& other) {
+		return (s.length() == other.s.length());
+	}
+
+	bool operator<(const PartiallySortedString& other) {
+		return (s.length() < other.s.length());
+	}
+
+	bool operator>(const PartiallySortedString& other) {
+		return (s.length() > other.s.length());
+	}
+};
+
 int main() {
-    cout << "Hello World!\n";
+	PartiallySortedString s1("abd");
+	PartiallySortedString s2("abc");
+
+    cout << (s1 != s2);
+	cout << (s1 == s2);
 }
