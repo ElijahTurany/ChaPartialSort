cf5711a42c9cd3385b241dbb7e6ecd2d1df47e64
Liam1017 <elijahturany1415@gmail.com>
2023-03-01 15:55:20 -0600

    2023/03/01 15:55

--- ChaPartialSort/ChaPartialSort.cpp
+++ ChaPartialSort/ChaPartialSort.cpp
@@ -11,11 +11,15 @@ public:
 		s = value;
 	}
 
+	PartiallySortedString() {
+		s = "";
+	}
+
 	bool operator==(const PartiallySortedString& other) {
 		return (other.s == s);
 	}
 
-	bool operator!=(const PartiallySortedString& other) {
+	bool operator|=(const PartiallySortedString& other) {
 		return (s.length() == other.s.length());
 	}
 
@@ -26,12 +30,70 @@ public:
 	bool operator>(const PartiallySortedString& other) {
 		return (s.length() > other.s.length());
 	}
+
+	string getVal() {
+		return s;
+	}
+};
+
+class PartiallySortedList {
+private:
+	PartiallySortedString* data;
+	int size{ 0 };
+	int capacity{ 10 };
+public:
+	PartiallySortedList() {
+		data = new PartiallySortedString[capacity];
+	}
+
+	void add( string value) {
+		if (size == capacity) {
+			this->expand();
+		}	
+		data[size++] = PartiallySortedString(value);
+	}
+
+	PartiallySortedString get(int index) {
+		if ((index < size) && (index >= 0)) {
+			return data[index];
+		}
+		throw "Out of bounds";
+	}
+
+	int getSize() {
+		return size;
+	}
+
+	void expand() {
+		PartiallySortedString* tmp = new PartiallySortedString[capacity * 2];
+		for (int i = 0; i < capacity; i++) {
+			tmp[i] = data[i];
+		}
+		delete[] data;
+		data = tmp;
+		tmp = nullptr;
+		capacity *= 2;
+	}
+
+	int countPermutations() {
+
+	}
 };
 
 int main() {
-	PartiallySortedString s1("abd");
-	PartiallySortedString s2("abc");
+	PartiallySortedList list;
 
-    cout << (s1 != s2);
-	cout << (s1 == s2);
+	list.add("k");
+	list.add("j");
+	list.add("i");
+	list.add("h");
+	list.add("g");
+	list.add("f");
+	list.add("e");
+	list.add("d");
+	list.add("c");
+	list.add("b");
+	list.add("a");
+
+	cout << list.get(10).getVal();
 }
